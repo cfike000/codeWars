a1705df0be7b2761be814ab3972cb88a40246b52
Carl E Fike <cafike2022@gmail.com>
2022-04-02 12:50:35 -0500

    FizzBuzz Kata
    Replace certain values however if any of the following conditions are met:
    
    If the value is a multiple of 3: use the value "Fizz" instead
    If the value is a multiple of 5: use the value "Buzz" instead
    If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead

---
+++ fizzbuzz.js
@@ -0,0 +1,17 @@
+// Return an array
+function fizzbuzz(n)
+{
+  let arr = []
+  for(i=1;i<=n;i++){
+  if(i % 3 == 0 && i % 5 == 0){
+    arr.push('Fizzbuzz')
+  }else if(i % 5 == 0){
+    arr.push('Buzz')
+  }else if(i % 3 == 0){
+    arr.push('Fizz')
+  }else{
+    arr.push(i)
+  }
+  }
+  return arr
+}

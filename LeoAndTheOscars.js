f90e9b78be9068456eb7755a5fd156580adddf00
Carl E Fike <cafike2022@gmail.com>
2022-05-23 13:10:32 -0500

    Leo And The Oscars Kata

---
+++ LeoAndTheOscars.js
@@ -0,0 +1,6 @@
+function leo(oscar){
+  return oscar == 88 ? "Leo finally won the oscar! Leo is happy" :
+  oscar == 86 ? "Not even for Wolf of wallstreet?!" :
+  oscar < 86 || oscar == 87 ? "When will you give Leo an Oscar?" :
+  oscar > 88 ? "Leo got one already!" : null
+}

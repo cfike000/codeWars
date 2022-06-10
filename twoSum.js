8aa6f2836bed528916413ead92966df1a4054aaf
Carl E Fike <cafike2022@gmail.com>
2022-06-10 12:58:50 -0500

    two Sum Kata
    Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).

---
+++ twoSum.js
@@ -0,0 +1,9 @@
+function twoSum(numbers, target) {
+  let seen = new Map();
+  for (let i = 0; i < numbers.length; i++) {
+    let x = numbers[i], y = target - x;
+    if (seen.has(y))
+      return [seen.get(y), i];
+    seen.set(x, i);
+  }
+}

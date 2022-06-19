ed01a7186b6d6d42e5ee8b553dc261f26a5178d7
Carl E Fike <cafike2022@gmail.com>
2022-06-19 12:37:33 -0500

    GADE cypher Kata

---
+++ GA-DE-RY-PO-LU-KI cypher.js
@@ -0,0 +1,11 @@
+const dict = {
+  G: "A", A: "G", D: "E", E: "D", 
+  R: "Y", Y: "R", P: "O", O: "P", 
+  L: "U", U: "L", K: "I", I: "K", 
+  g: "a", a: "g", d: "e", e: "d", 
+  r: "y", y: "r", p: "o", o: "p", 
+  l: "u", u: "l", k: "i", i: "k", 
+}
+
+const encode = s => s.replace(/./g, c => dict[c] || c)
+const decode = encode
